d10e89c823e12891e96ea4bfb1823627a855c851
Brik Royster <elkemore@gmail.com>
2019-03-04 20:21:20 -0800

    ng material bones

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MaterialModule} from './material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent,
+  ]
+})
+export class AppModule {
+}
