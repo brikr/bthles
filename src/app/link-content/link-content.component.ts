abeb737c3c7007197cf715dadf299407223eea49
Brik Royster <elkemore@gmail.com>
2019-03-07 23:35:04 -0800

    Basic link content page
    Redirects to location or back to home page if link isn't valid
    resolve #6

--- src/app/link-content/link-content.component.ts
+++ src/app/link-content/link-content.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
 import {ActivatedRoute, ParamMap} from '@angular/router';
+import {environment} from '@bthles-environment/environment';
+import {Link} from '@bthles-types/types';
 
 @Component({
   selector: 'bthles-link-content',
@@ -11,11 +14,22 @@ export class LinkContentComponent {
 
   constructor(
       private readonly route: ActivatedRoute,
+      private readonly db: AngularFirestore,
   ) {
-    this.route.paramMap.subscribe((params: ParamMap) => {
+    this.route.paramMap.subscribe(async (params: ParamMap) => {
       // short param is definitely defined because we can only here from the
       // route with short defined
       this.linkId = params.get('short')!;
+      const snapshot = (await this.db.collection('links')
+                            .doc<Link>(params.get('short')!)
+                            .ref.get());
+      const dest = snapshot.data() as Link | undefined;
+
+      if (dest !== undefined) {
+        window.location.replace(dest.content);
+      } else {
+        window.location.replace(environment.baseUrl);
+      }
     });
   }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,19 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatInputModule, MatProgressBarModule, MatSnackBarModule} from '@angular/material';
+import {MatButtonModule, MatInputModule, MatProgressBarModule, MatProgressSpinnerModule, MatSnackBarModule} from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatInputModule,
     MatProgressBarModule,
+    MatProgressSpinnerModule,
     MatSnackBarModule,
   ],
   exports: [
     MatButtonModule,
     MatInputModule,
     MatProgressBarModule,
+    MatProgressSpinnerModule,
     MatSnackBarModule,
   ],
 })
