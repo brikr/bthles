af0d31a8c59e894e2fd26695e295956d6dcab51e
Brik Royster <elkemore@gmail.com>
2019-03-08 18:27:48 -0800

    Add staging and prod environments/aliases
    As usual, these keys are safe to keep in the repo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://bthl.es',
+  firebase: {
+    apiKey: 'AIzaSyB_4x0LdALGFE04e0y524rjpy1Rc2k7PhU',
+    authDomain: 'bthles.firebaseapp.com',
+    databaseURL: 'https://bthles.firebaseio.com',
+    projectId: 'bthles',
+    storageBucket: 'bthles.appspot.com',
+    messagingSenderId: '312510828218'
+  },
+  // TODO(brikr): When the SSL cert is issued, change this to HTTPS
+  baseUrl: 'http://bthl.es',
 };
